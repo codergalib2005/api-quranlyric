e239da3c2a6cba900025a79342c14ae8191a4c4d
codergalib20 <coderboygalif@gmail.com>
2023-05-19 20:16:38 +0600

    runnig express application

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,12 @@ import express from "express";
 const app = express();
 
 // middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// main page
+app.get("/", (req, res) => {
+  res.send(`<h2>Blogger Application</h2>`);
+});
 
 export default app;
