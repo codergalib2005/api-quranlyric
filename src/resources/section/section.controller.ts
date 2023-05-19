57c66acb86561a490dee18382e2842d65d5ad872
codergalib20 <coderboygalif@gmail.com>
2023-05-19 20:49:21 +0600

    section routes

--- src/resources/section/section.controller.ts
+++ src/resources/section/section.controller.ts
@@ -3,3 +3,11 @@ const getASection = (req, res) => {};
 const getSections = (req, res) => {};
 const updateASection = (req, res) => {};
 const deleteASection = (req, res) => {};
+
+export {
+  createSection,
+  getASection,
+  getSections,
+  updateASection,
+  deleteASection,
+};
--- src/resources/section/section.route.ts
+++ src/resources/section/section.route.ts
@@ -1,5 +1,21 @@
 import express from "express";
-
+import {
+  updateASection,
+  createSection,
+  getSections,
+  getASection,
+  deleteASection,
+} from "./section.controller";
 const router = express.Router();
+
+// with default route /api/v1/section
+router.route("/").get(getSections).post(createSection);
+
+// with ID=>(id) params
+router
+  .route("/:id")
+  .get(getASection)
+  .patch(updateASection)
+  .delete(deleteASection);
 
 export default router;
