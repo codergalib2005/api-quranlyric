037803fe61f54a0ab6478010224d00a84ebf4dd9
codergalib20 <coderboygalif@gmail.com>
2023-05-19 21:31:15 +0600

    section routes

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import sections from "./resources/section/section.route";
 import express from "express";
 
 const app = express();
@@ -11,6 +12,11 @@ app.get("/", (req, res) => {
   res.send(`<h2>Blogger Application</h2>`);
 });
 
-// 
+//
+
+app.use("/api/v1/sections", sections);
+app.get("/api/v1", (req, res) => {
+  console.log("hello");
+});
 
 export default app;
