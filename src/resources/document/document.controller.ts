a7054a409bd0757433e4b3f93bcac5ec30239413
codergalib20 <coderboygalif@gmail.com>
2023-05-19 22:33:18 +0600

    documents model

---
+++ src/resources/document/document.controller.ts
@@ -0,0 +1,5 @@
+export const createDoc = async (req, res) => {};
+export const getDocs = async (req, res) => {};
+export const getADoc = async (req, res) => {};
+export const updateDoc = async (req, res) => {};
+export const deleteDoc = async (req, res) => {};
