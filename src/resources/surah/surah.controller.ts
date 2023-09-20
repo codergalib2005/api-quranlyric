9cf1702d33a0a0910a51fc2c1fc7ffc2341535e1
codergalib2005 <coderboygalif@gmail.com>
2023-09-20 21:34:37 +0600

    updated

--- src/resources/surah/surah.controller.ts
+++ src/resources/surah/surah.controller.ts
@@ -4,7 +4,11 @@ const getAllSurah = async (req, res) => {
   try {
     const count = await Surah.countDocuments();
     const findSurah = await Surah.find({});
-    res.send({ count, findSurah });
+    if (!findSurah) {
+      res.status(404).json({ msg: "Surah not found!!" });
+    } else {
+      res.status(201).json({ message: "Surah Loaded", count, data: findSurah });
+    }
   } catch (err) {
     res.status(500).json({ error: "Failed to create section", err });
   }
