534e5d922197e3a77f3f99588eea28982427860b
codergalib2005 <coderboygalif@gmail.com>
2023-09-26 00:13:02 +0600

    update surah schema

--- src/resources/surah/surah.model.ts
+++ src/resources/surah/surah.model.ts
@@ -2,13 +2,101 @@ import mongoose, { Schema, Document } from "mongoose";
 
 interface IVerses {
   audio_one: string;
-  verses: 1;
+  verses: number;
   english?: string;
   text: string;
-  ar?: {
+  //  translation
+  ar: {
     text: string;
   };
-  en?: {
+  en: {
+    text: string;
+  };
+  az: {
+    text: string;
+  };
+  bg: {
+    text: string;
+  };
+  bn: {
+    text: string;
+  };
+  bs: {
+    text: string;
+  };
+  cs: {
+    text: string;
+  };
+  de: {
+    text: string;
+  };
+  dv: {
+    text: string;
+  };
+  es: {
+    text: string;
+  };
+  fa: {
+    text: string;
+  };
+  fr: {
+    text: string;
+  };
+  ha: {
+    text: string;
+  };
+  ja: {
+    text: string;
+  };
+  ko: {
+    text: string;
+  };
+  ku: {
+    text: string;
+  };
+  ml: {
+    text: string;
+  };
+  ms: {
+    text: string;
+  };
+  nl: {
+    text: string;
+  };
+  no: {
+    text: string;
+  };
+  pl: {
+    text: string;
+  };
+  pt: {
+    text: string;
+  };
+  ro: {
+    text: string;
+  };
+  ru: {
+    text: string;
+  };
+  sd: {
+    text: string;
+  };
+  so: {
+    text: string;
+  };
+  sq: {
+    text: string;
+  };
+  sv: {
+    text: string;
+  };
+  hi: {
+    text: string;
+  };
+  id_l: {
+    text: string;
+  };
+  it: {
     text: string;
   };
 }
@@ -70,6 +158,7 @@ const SurahSchema: Schema = new Schema(
           type: String,
           required: true,
         },
+        // translation:
         ar: {
           text: {
             type: String,
@@ -80,6 +169,154 @@ const SurahSchema: Schema = new Schema(
             type: String,
           },
         },
+        az: {
+          text: {
+            type: String,
+          },
+        },
+        bg: {
+          text: {
+            type: String,
+          },
+        },
+        bn: {
+          text: {
+            type: String,
+          },
+        },
+        bs: {
+          text: {
+            type: String,
+          },
+        },
+        cs: {
+          text: {
+            type: String,
+          },
+        },
+        de: {
+          text: {
+            type: String,
+          },
+        },
+        dv: {
+          text: {
+            type: String,
+          },
+        },
+        es: {
+          text: {
+            type: String,
+          },
+        },
+        fa: {
+          text: {
+            type: String,
+          },
+        },
+        fr: {
+          text: {
+            type: String,
+          },
+        },
+        ha: {
+          text: {
+            type: String,
+          },
+        },
+        ja: {
+          text: {
+            type: String,
+          },
+        },
+        ko: {
+          text: {
+            type: String,
+          },
+        },
+        ku: {
+          text: {
+            type: String,
+          },
+        },
+        ml: {
+          text: {
+            type: String,
+          },
+        },
+        ms: {
+          text: {
+            type: String,
+          },
+        },
+        nl: {
+          text: {
+            type: String,
+          },
+        },
+        no: {
+          text: {
+            type: String,
+          },
+        },
+        pl: {
+          text: {
+            type: String,
+          },
+        },
+        pt: {
+          text: {
+            type: String,
+          },
+        },
+        ro: {
+          text: {
+            type: String,
+          },
+        },
+        ru: {
+          text: {
+            type: String,
+          },
+        },
+        sd: {
+          text: {
+            type: String,
+          },
+        },
+        so: {
+          text: {
+            type: String,
+          },
+        },
+        sq: {
+          text: {
+            type: String,
+          },
+        },
+        sv: {
+          text: {
+            type: String,
+          },
+        },
+
+        //
+        hi: {
+          text: {
+            type: String,
+          },
+        },
+        id_l: {
+          text: {
+            type: String,
+          },
+        },
+        it: {
+          text: {
+            type: String,
+          },
+        },
+        // the end of the object
       },
     ],
   },
