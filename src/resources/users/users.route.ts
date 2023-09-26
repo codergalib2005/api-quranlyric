3e2d51088f478725aefab0c4ab08b117402066b3
codergalib2005 <coderboygalif@gmail.com>
2023-09-26 22:47:37 +0600

    updating api

--- src/resources/users/users.route.ts
+++ src/resources/users/users.route.ts
@@ -2,3 +2,9 @@ import { register, login, googleLogin } from "./users.controller";
 import express from "express";
 
 const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/google-login", googleLogin);
+
+export default router;
